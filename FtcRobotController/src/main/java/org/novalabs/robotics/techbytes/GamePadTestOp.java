a889a02d772576a07af78d3f07a323c1933107de
Tyler Kim <tyler.ys.kim@gmail.com>
2015-10-14 00:06:15 -0400

    Initial Code

---
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/GamePadTestOp.java
@@ -0,0 +1,58 @@
+package org.novalabs.robotics.techbytes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * TeleOp Mode
+ * <p/>
+ * Enables control of the robot via the gamepad
+ */
+public class GamePadTestOp extends OpMode {
+
+    private String startDate;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor motorOne;
+
+    public void init() {
+        motorOne = hardwareMap.dcMotor.get("motor_1");
+    }
+
+
+    /*
+       * Code to run when the op mode is first enabled goes here
+       * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+       */
+    @Override
+    public void init_loop() {
+        startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+        runtime.reset();
+        telemetry.addData("Null Op Init Loop", runtime.toString());
+
+
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("1 Start", "NullOp started at " + startDate);
+        telemetry.addData("2 Status", "running for " + runtime.toString());
+        if (gamepad1.a) {
+            motorOne.setPower(.5);
+        } else {
+            motorOne.setPower(0);
+        }
+    }
+
+
+}
+
+
---
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/MotorTestOp.java
@@ -0,0 +1,67 @@
+package org.novalabs.robotics.techbytes;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * TeleOp Mode
+ * <p/>
+ * Enables control of the robot via the gamepad
+ */
+public class MotorTestOp extends OpMode {
+    private long startTime = -1;
+    private String startDate;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor motorOne;
+    private DcMotorController controler;
+
+    @Override
+    public void init() {
+        motorOne = hardwareMap.dcMotor.get("motor_1");
+        motorOne.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
+        controler = hardwareMap.dcMotorController.get("motor_controller_1");
+        controler.setMotorChannelMode(1, DcMotorController.RunMode.RUN_USING_ENCODERS);
+
+    }
+
+    /*
+       * Code to run when the op mode is first enabled goes here
+       * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+       */
+    @Override
+    public void init_loop() {
+        startDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(new Date());
+        runtime.reset();
+        telemetry.addData("Null Op Init Loop", runtime.toString());
+        startTime = -1;
+    }
+
+    /*
+     * This method will be called repeatedly in a loop
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
+     */
+    @Override
+    public void loop() {
+        int encoderValue = motorOne.getCurrentPosition();
+        telemetry.addData("1 Start", "NullOp started at " + startDate);
+        telemetry.addData("2 Status", "running for " + runtime.toString());
+        if (startTime == -1) {
+            startTime = System.currentTimeMillis();
+            motorOne.setPower(.5);
+        }
+//    if(System.currentTimeMillis ()>startTime+3500) {
+//      motorOne.setPower(0);
+//      motorOne.getCurrentPosition();
+//    }
+        telemetry.addData("EncoderValue", "encoder value: " + encoderValue);
+
+    }
+}
+
