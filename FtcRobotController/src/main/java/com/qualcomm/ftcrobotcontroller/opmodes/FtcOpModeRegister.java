0d97f2a7e6d4326e8c764a1a4097398d1f5b0f67
Zach Shuster <zachnicosammy@gmail.com>
2015-10-15 20:05:31 -0400

    test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -34,6 +34,8 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
+import org.novalabs.robotics.techbytes.MotorTestOp;
+
 /**
  * Register Op Modes
  */
@@ -61,7 +63,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
-
+    manager.register("MotorTestOp", MotorTestOp.class);
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
