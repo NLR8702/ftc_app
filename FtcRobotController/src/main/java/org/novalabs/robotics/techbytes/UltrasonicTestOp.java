6bf4f293d995913d0884f8ce03cfa6075f07f0b8
Andrew <andrewrbolster@gmail.com>
2015-10-26 11:57:45 -0400

    UltrasonicTestOp

---
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/UltrasonicTestOp.java
@@ -0,0 +1,27 @@
+package org.novalabs.robotics.techbytes;
+
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Andrew Bolster on 10/25/2015.
+ */
+public class UltrasonicTestOp extends OpMode {
+
+    private UltrasonicSensor ultrasonicSensor;
+
+    @Override
+    public void init() {
+ultrasonicSensor = hardwareMap.ultrasonicSensor.get("Ult_1");
+    }
+
+    @Override
+    public void loop() {
+ultrasonicSensor.getUltrasonicLevel();
+        telemetry.addData("", "Ultrasonic Level: " + ultrasonicSensor);
+
+    }
+
+
+
+}
