a9ea6b6f9911c0f6f8a86e3985101118741c8752
Tyler S <tmanzz122@gmail.com>
2015-10-29 19:04:21 -0400

    Created the Opitcal tester and registered it

---
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/TS_OpticalDistanceIsFun.java
@@ -0,0 +1,18 @@
+package org.novalabs.robotics.techbytes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by tmanz on 10/29/2015.
+ */
+    public class TS_OpticalDistanceIsFun extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
