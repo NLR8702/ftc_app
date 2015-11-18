3138e9c9fe7a0b17e75561a80ad4ba9f3a11faff
Tyler Kim <tyler.ys.kim@gmail.com>
2015-11-17 19:41:47 -0500

    disabled the light

--- FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/TK_ColorOpMode.java
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/TK_ColorOpMode.java
@@ -5,18 +5,27 @@ import android.graphics.Color;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
 
 /**
  * Created by tylerkim on 10/25/15.
  */
 public class TK_ColorOpMode extends OpMode{
+    private static final int LED_CHANNEL = 5;
     ColorSensor sensorRGB;
     DeviceInterfaceModule cdim;
     ColorAnalyzer analyzer = new ColorAnalyzer();
     int debug_count = 0;
+    private boolean bEnabled = false;
+
     @Override
     public void init() {
         sensorRGB = hardwareMap.colorSensor.get("color");
+        cdim = hardwareMap.deviceInterfaceModule.get("dim");
+        cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannelController.Mode.OUTPUT);
+        cdim.setDigitalChannelState(LED_CHANNEL, bEnabled);
+
+
     }
 
     @Override
@@ -27,13 +36,15 @@ public class TK_ColorOpMode extends OpMode{
         int green = sensorRGB.green();
         int alpha = sensorRGB.alpha();
 
-//        telemetry.addData("TEST ", debug_count);
-//        telemetry.addData("TKS_red",sensorRGB.red());
-//        telemetry.addData("TKS _blue",sensorRGB.blue());
-//        telemetry.addData("TKS_green",sensorRGB.green());
-//        telemetry.addData("TKS_clear",sensorRGB.alpha());
-          //telemetry.addData("detecting: ",  analyzer.Color_Result(red,blue, green, alpha));
-        ColorTypes result = analyzer.Color_Result(red,blue, green, alpha);
-        telemetry.addData("detecting: ", result);
+
+        float[] percentageArray = analyzer.getPercentage(red, blue, green);
+        telemetry.addData("TEST ", debug_count);
+        telemetry.addData("TKS_red",percentageArray[1]);
+        telemetry.addData("TKS _blue",percentageArray[0]);
+        telemetry.addData("TKS_green",percentageArray[2]);
+        //telemetry.addData("TKS_clear",sensorRGB.alpha());
+        //telemetry.addData("detecting: ",  analyzer.Color_Result(red,blue, green, alpha));
+        //ColorTypes result = analyzer.Color_Result(red,blue, green, alpha);
+        //telemetry.addData("detecting: ", result);
     }
 }
