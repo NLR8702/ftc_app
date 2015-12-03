a580b0af8278ee0b656335a6c177f77ecad2eb7f
Tyler Kim <tyler.ys.kim@gmail.com>
2015-12-03 18:37:18 -0500

    DumperHardwareController 1.0

--- FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/DumperHardwareController.java
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/DumperHardwareController.java
@@ -1,13 +1,18 @@
 package org.novalabs.robotics.techbytes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by tylerkim on 12/1/15.
  */
 public class DumperHardwareController implements HardwareController {
+    private Servo DumperMotor;
+    private Servo RightSweeperMotor;
+    private Servo LeftSweeperMotor;
     @Override
     public void init(OpMode opMode) {
+
 
     }
 
