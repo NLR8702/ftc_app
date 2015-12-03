d57ec8bcbce57b6b2d6c8de32e2d9ab59655979c
Zach Shuster <zachnicosammy@gmail.com>
2015-12-03 18:32:08 -0500

    Merge branch 'master' of https://github.com/daviditkin/ftc_app
    # Conflicts:
    #	FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/GamePadTestOp.java

--- FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/TapeHardwareController.java
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/TapeHardwareController.java
@@ -10,29 +10,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class TapeHardwareController implements HardwareController {
     private DcMotor TapeMotor;
     private Servo tapeGuide;
+    private float tapePosition;
     @Override
     public void init(OpMode opMode) {
         TapeMotor = opMode.hardwareMap.dcMotor.get("tapeMotor");
         tapeGuide = opMode.hardwareMap.servo.get("tapeGuide");
-
+        tapePosition=.5f;
 
     }
 
     @Override
     public void loop(OpMode opMode) {
-        if(opMode.gamepad2.a){
+        if (opMode.gamepad2.a) {
             TapeMotor.setPower(-0.5);
-        } else if(opMode.gamepad2.y){
+        } else if (opMode.gamepad2.y) {
             TapeMotor.setPower(0.5);
         } else
             TapeMotor.setPower(0);
-        if(opMode.gamepad2.dpad_up){
-            tapeGuide.setPosition(1);
-        } else if(opMode.gamepad2.dpad_down){
-            tapeGuide.setPosition(0);
+
+
+        if (gamepad2.b && tapePosition < .9) {
+            tapePosition = tapePosition + .1f;
+
+        } else if (gamepad2.x && tapePosition > .1) {
+            tapePosition = tapePosition - .1f;
         }
-
-
-
+        tapeGuide.setPosition(tapePosition);
     }
-}
+    }
+
