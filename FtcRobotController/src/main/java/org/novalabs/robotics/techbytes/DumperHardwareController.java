728ce6b4e60f2ac5b880fa3117105ff9a418f62d
Tyler Kim <tyler.ys.kim@gmail.com>
2015-12-03 19:25:00 -0500

    DumperHardwareController 1.2

--- FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/DumperHardwareController.java
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/DumperHardwareController.java
@@ -10,14 +10,47 @@ public class DumperHardwareController implements HardwareController {
     private Servo DumperMotor;
     private Servo RightSweeperMotor;
     private Servo LeftSweeperMotor;
+    private float LeftSweeperPosition;
+    private float RightSweeperPosition;
     @Override
     public void init(OpMode opMode) {
-
+        DumperMotor = opMode.hardwareMap.servo.get("dumpMotor");
+        RightSweeperMotor = opMode.hardwareMap.servo.get("rightSweeperMotor");
+        LeftSweeperMotor = opMode.hardwareMap.servo.get("leftSweeperMotor");
+        RightSweeperPosition = 1f;
+        LeftSweeperPosition = 0f;
+        // :)
 
     }
 
     @Override
     public void loop(OpMode opMode) {
 
+
+        if (opMode.gamepad2.dpad_up){
+            DumperMotor.setPosition(1);
+        }else if (opMode.gamepad2.dpad_down){
+            DumperMotor.setPosition(0);
+        }
+
+        if (opMode.gamepad2.dpad_left && RightSweeperPosition < .9) {
+            RightSweeperPosition = RightSweeperPosition + .1f;
+
+        } else if (!opMode.gamepad2.dpad_left) {
+            RightSweeperPosition-=.1f;
+        }
+        RightSweeperMotor.setPosition(RightSweeperPosition);
+
+        if (opMode.gamepad2.dpad_right && LeftSweeperPosition > .1) {
+            LeftSweeperPosition = LeftSweeperPosition - .1f;
+
+        } else if (!opMode.gamepad2.dpad_right){
+            LeftSweeperMotor.setPosition(1);
+        }
+        LeftSweeperMotor.setPosition(LeftSweeperPosition);
+
+
+
     }
 }
+//
