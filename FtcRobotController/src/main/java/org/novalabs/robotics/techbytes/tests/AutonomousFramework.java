174c66cf52a79780dbe50c163667d8c5fdcd79fa
Tyler Kim <tyler.ys.kim@gmail.com>
2015-12-06 16:21:02 -0500

    NEW Autonomous Framework

---
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/tests/AutonomousFramework.java
@@ -0,0 +1,37 @@
+package org.novalabs.robotics.techbytes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tylerkim on 12/1/15.
+ */
+public class FrameworkTestOpMode extends OpMode {
+    private List<HardwareController> controllerList = new ArrayList<HardwareController>();
+    public FrameworkTestOpMode(){
+        controllerList.add(new TapeHardwareController());
+        controllerList.add(new DriveHardwareController());
+        controllerList.add(new DumperHardwareController());
+        controllerList.add(new KickstandHardwareController());
+
+    }
+
+    @Override
+    public void init() {
+        for(HardwareController aController : controllerList) {
+            aController.init(this);
+        }
+
+    }
+
+    @Override
+    public void loop() {
+        for(HardwareController aController : controllerList) {
+            aController.loop(this);
+        }
+
+    }
+}
+
