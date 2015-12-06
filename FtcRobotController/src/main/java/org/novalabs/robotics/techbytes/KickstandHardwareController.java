093ae02f1c680cc84ddcaddae73f82018a1f7cc9
Tyler Kim <tyler.ys.kim@gmail.com>
2015-12-06 15:03:27 -0500

    Experiment with encoders on the kickstand motor.

--- FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/KickstandHardwareController.java
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/KickstandHardwareController.java
@@ -2,6 +2,7 @@ package org.novalabs.robotics.techbytes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
  * Created by tylerkim on 12/5/15.
@@ -9,19 +10,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  */
 public class KickstandHardwareController implements HardwareController {
     private DcMotor KickstandMotor;
+    private int loopRotations=0;
 
     @Override
     public void init(OpMode opMode) {
-        KickstandMotor = opMode.hardwareMap.dcMotor.get("KickstandMotor");
+        KickstandMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
 
     }
-
     @Override
     public void loop(OpMode opMode) {
-        if(opMode.gamepad1.left_bumper) {
-            KickstandMotor.setTargetPosition(1);
-        }
-        else KickstandMotor.setPower(0);
+        opMode.telemetry.addData("loopRotations: ", loopRotations);
+        loopRotations = loopRotations + 1;
+        if (loopRotations == 1) {
+            KickstandMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        } else if (loopRotations == 2) {
+            KickstandMotor.setTargetPosition(6 * 560);
+        } else if (loopRotations == 3) {
+            KickstandMotor.setPower(.25);
 
+
+        }
     }
 }
