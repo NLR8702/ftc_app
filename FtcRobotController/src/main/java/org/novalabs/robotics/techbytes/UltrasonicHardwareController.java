3db4ba57ac8420ffa9b6698b3836efe0e5b8ce7e
Tyler Kim <tyler.ys.kim@gmail.com>
2015-12-28 22:10:23 -0500

    Initial Ultrasonic Sensor hardware controller

---
+++ FtcRobotController/src/main/java/org/novalabs/robotics/techbytes/UltrasonicHardwareController.java
@@ -0,0 +1,18 @@
+package org.novalabs.robotics.techbytes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by tylerkim on 12/28/15.
+ */
+public class UltrasonicHardwareController implements HardwareController {
+    @Override
+    public void init(OpMode opMode) {
+
+    }
+
+    @Override
+    public void loop(OpMode opMode) {
+
+    }
+}
