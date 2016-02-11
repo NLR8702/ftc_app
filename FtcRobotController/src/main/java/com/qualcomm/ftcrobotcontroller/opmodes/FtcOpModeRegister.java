b48896ea1eec45899196d5040c6dadf043df3135
ditkin <ditkin@eucleo.com>
2016-02-11 12:35:23 -0500

    Added Andrew's UltranosicTestOp to the FtcOpModeRegister

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -36,6 +36,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
 import org.novalabs.robotics.techbytes.AutonomousModeRight;
 import org.novalabs.robotics.techbytes.FrameworkTestOpMode;
+import org.novalabs.robotics.techbytes.UltrasonicTestOp;
 import org.novalabs.robotics.techbytes.tests.AutonomousSleep;
 
 /**
@@ -96,5 +97,6 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("PushBotDriveTouch", PushBotDriveTouch.java);
     //manager.register("PushBotIrSeek", PushBotIrSeek.java);
     //manager.register("PushBotSquare", PushBotSquare.java);
+    manager.register("UltrasonicTest", UltrasonicTestOp.class);
   }
 }
